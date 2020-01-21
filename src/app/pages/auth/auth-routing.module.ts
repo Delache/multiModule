27b35b83b66d635bd1badc5ed4eb4e3c27520266
Delache <erique.delacharlerie@gmail.com>
2020-01-21 11:40:45 +0100

    quete multimodules

---
+++ src/app/pages/auth/auth-routing.module.ts
@@ -0,0 +1,19 @@
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'signin' },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
